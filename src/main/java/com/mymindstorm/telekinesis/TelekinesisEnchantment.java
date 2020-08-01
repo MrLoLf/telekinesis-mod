bb3d0a8088cf605179d1c53a7a30053506b856fa
Brendan Early <mymindstorm@evermiss.net>
2020-08-01 12:25:04 -0500

    Fix not being able to enchant weapons or books

--- src/main/java/com/mymindstorm/telekinesis/TelekinesisEnchantment.java
+++ src/main/java/com/mymindstorm/telekinesis/TelekinesisEnchantment.java
@@ -6,7 +6,7 @@ import net.minecraft.entity.EquipmentSlot;
 
 public class TelekinesisEnchantment extends Enchantment {
     public TelekinesisEnchantment() {
-        super(Rarity.UNCOMMON, EnchantmentTarget.DIGGER, new EquipmentSlot[] {EquipmentSlot.MAINHAND});
+        super(Rarity.UNCOMMON, EnchantmentTarget.BREAKABLE, new EquipmentSlot[] {EquipmentSlot.MAINHAND});
     }
 
     public int getMinPower(int level) {
