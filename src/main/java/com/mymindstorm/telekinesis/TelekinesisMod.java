b133b6cab9ba6501f0e109c9bdc157d964a8415e
Temac000 <temac000@bk.ru>
2020-08-01 15:44:00 +0300

    Added compatibility with tall flowers/crops

--- src/main/java/com/mymindstorm/telekinesis/TelekinesisMod.java
+++ src/main/java/com/mymindstorm/telekinesis/TelekinesisMod.java
@@ -3,6 +3,8 @@ package com.mymindstorm.telekinesis;
 import com.mymindstorm.telekinesis.event.BlockItemDropEvent;
 import com.mymindstorm.telekinesis.event.EntityItemDropEvent;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
@@ -10,7 +12,12 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.context.LootContextTypes;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+
+import java.util.Arrays;
 
 import static net.minecraft.block.Block.dropStack;
 
@@ -21,6 +28,17 @@ public class TelekinesisMod implements ModInitializer {
 			new TelekinesisEnchantment()
 	);
 
+	// iterate and add to inventory until different block
+	private void tallBlock(World world, BlockPos origBlock, Direction direction, Block blockid, PlayerEntity player) {
+		BlockPos pos = origBlock.offset(direction);
+		while (world.getBlockState(pos).getBlock().equals(blockid)) {
+			ItemStack drop = new ItemStack(world.getBlockState(pos).getBlock());
+			player.inventory.insertStack(drop);
+			world.breakBlock(pos, false);
+			pos = pos.offset(direction);
+		}
+	}
+
 	@Override
 	public void onInitialize() {
 		BlockItemDropEvent.EVENT.register((state, world, pos, blockEntity, entity, stack, drops) -> {
@@ -29,6 +47,26 @@ public class TelekinesisMod implements ModInitializer {
 				PlayerEntity player = world.getPlayerByUuid(entity.getUuid());
 				if (player == null) {
 					return true;
+				}
+
+				// TODO: add config file instead of hard coding
+				Block[] BreakUp = {
+						Blocks.SUGAR_CANE,
+						Blocks.BAMBOO,
+						Blocks.CACTUS
+				};
+				Block[] BreakDown = {
+						Blocks.PEONY,
+						Blocks.SUNFLOWER,
+						Blocks.LILAC,
+						Blocks.ROSE_BUSH
+				};
+				// check for tall blocks
+				Block blockid = state.getBlock();
+				if (Arrays.asList(BreakUp).contains(blockid)) {
+					tallBlock(world, pos, Direction.UP, blockid, player);
+				} else if (Arrays.asList(BreakDown).contains(blockid)) {
+					tallBlock(world, pos, Direction.DOWN, blockid, player);
 				}
 
 				// insert drops into inventory
